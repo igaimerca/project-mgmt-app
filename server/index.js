92a7a184173e8a9e7f9c5fede43c9ba2bafcf778
igaimerca <igaimerca@gmail.com>
2022-10-05 13:09:51 +0200

    crud operations

--- server/index.js
+++ server/index.js
@@ -1,21 +1,25 @@
-const express = require("express")
-const colors = require("colors")
-require("dotenv").config()
-const { graphqlHTTP } = require("express-graphql")
-const schema = require('./schema/schema')
-const { connectDB } = require("./config/db")
+const express = require('express');
+const colors = require('colors');
+const cors = require('cors');
+require('dotenv').config();
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema');
+const connectDB = require('./config/db');
+const port = process.env.PORT || 5000;
 
-const app = express()
+const app = express();
 
 // Connect to database
 connectDB();
 
-app.use("/graphql", graphqlHTTP({
-    schema,
-    graphiql: process.env.NODE_ENV === 'development'
-}))
+app.use(cors());
 
-const PORT  = process.env.PORT || 5000
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-})
+app.use(
+    '/graphql',
+    graphqlHTTP({
+        schema,
+        graphiql: process.env.NODE_ENV === 'development',
+    })
+);
+
+app.listen(port, console.log(`Server running on port ${port}`));
